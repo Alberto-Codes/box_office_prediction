6c3a25cc7bc4bbd6c576501a4a9f13dff2cce3c3
Alberto Nieto <94092485+Alberto-Codes@users.noreply.github.com>
2024-05-07 01:00:17 +0000

    refactor: streamline imports and enhance documentation in app.py, setup_gcs.py
    - In app.py, removed unused imports and refined comments and documentation for clarity.
    - Simplified the download and upload functions to enhance readability and maintainability.
    - In setup_gcs.py, streamlined bucket and BigQuery dataset creation functions, improving documentation for better clarity.
    - Updated table schema creation to use non-repeated fields, aligning with current data structure needs.

--- src/api/app.py
+++ src/api/app.py
@@ -1,16 +1,11 @@
-import gzip
 import hashlib
-import io
-import json
 import os
-from concurrent.futures import ThreadPoolExecutor
-from typing import List
 
-import httpx
 from config import Config
 from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException
 from google.cloud import bigquery, storage
+import httpx
 
 app = FastAPI()
 
@@ -29,27 +24,24 @@ dataset_files = [
 @app.post("/download-data/")
 async def download_data():
     """
-    Download multiple IMDb dataset files and upload them to Google Cloud Storage.
+    Downloads IMDb dataset files and uploads them to GCS.
 
-    This endpoint initiates the downloading of dataset files from IMDb and uploads
-    them to a specified Google Cloud Storage bucket, organizing them within a
-    directory based on the project ID hash.
+    This endpoint triggers the download of IMDb dataset files from a URL and
+    uploads them to a specific GCS bucket organized by a hash of the project ID.
 
     Raises:
-        HTTPException: If the Google Cloud Project ID is not configured or if there
-        is an error in downloading or uploading the data.
+        HTTPException: If GCP Project ID is missing or an error occurs in data
+        transfer processes.
 
     Returns:
-        dict: Confirmation message indicating successful data transfer.
+        dict: Confirmation of successful data transfer.
     """
     load_dotenv()  # Load environment variables from the .env file.
 
-    # Get GCP project ID from config
     project_id = Config.GOOGLE_CLOUD_PROJECT
     if not project_id:
         raise HTTPException(status_code=500, detail="GCP Project ID is not configured.")
 
-    # Initialize Google Cloud Storage client
     storage_client = storage.Client(project=project_id)
     project_hash = hashlib.blake2b(project_id.encode(), digest_size=8).hexdigest()
     bucket_name = f"{project_hash}-imdb-datasets"
@@ -57,11 +49,8 @@ async def download_data():
 
     async with httpx.AsyncClient() as client:
         for file_name in dataset_files:
-            # Construct the URL of the IMDb dataset to download
             dataset_url = f"https://datasets.imdbws.com/{file_name}"
-
             try:
-                # Download dataset
                 response = await client.get(dataset_url)
                 response.raise_for_status()
                 data_content = response.content
@@ -70,74 +59,27 @@ async def download_data():
                     status_code=500, detail=f"Error downloading data: {str(exc)}"
                 )
 
-            # Upload the downloaded data to Google Cloud Storage
             blob = bucket.blob(f"raw-datasets/{file_name}")
             blob.upload_from_string(data_content, content_type="application/gzip")
 
     return {"message": "Data downloaded and uploaded successfully to GCS"}
 
 
-# # Define the schema for each IMDb file
-# schema_definitions = {
-#     "name.basics": [
-#         bigquery.SchemaField("nconst", "STRING"),
-#         bigquery.SchemaField("primaryName", "STRING"),
-#         bigquery.SchemaField("birthYear", "INTEGER"),
-#         bigquery.SchemaField("deathYear", "INTEGER"),
-#         bigquery.SchemaField("primaryProfession", "STRING", mode="REPEATED"),
-#         bigquery.SchemaField("knownForTitles", "STRING", mode="REPEATED"),
-#     ],
-#     "title.akas": [
-#         bigquery.SchemaField("titleId", "STRING"),
-#         bigquery.SchemaField("ordering", "INTEGER"),
-#         bigquery.SchemaField("title", "STRING"),
-#         bigquery.SchemaField("region", "STRING"),
-#         bigquery.SchemaField("language", "STRING"),
-#         bigquery.SchemaField("types", "STRING", mode="REPEATED"),
-#         bigquery.SchemaField("attributes", "STRING", mode="REPEATED"),
-#         bigquery.SchemaField("isOriginalTitle", "BOOLEAN"),
-#     ],
-#     "title.basics": [
-#         bigquery.SchemaField("tconst", "STRING"),
-#         bigquery.SchemaField("titleType", "STRING"),
-#         bigquery.SchemaField("primaryTitle", "STRING"),
-#         bigquery.SchemaField("originalTitle", "STRING"),
-#         bigquery.SchemaField("isAdult", "BOOLEAN"),
-#         bigquery.SchemaField("startYear", "INTEGER"),
-#         bigquery.SchemaField("endYear", "INTEGER"),
-#         bigquery.SchemaField("runtimeMinutes", "INTEGER"),
-#         bigquery.SchemaField("genres", "STRING", mode="REPEATED"),
-#     ],
-#     "title.crew": [
-#         bigquery.SchemaField("tconst", "STRING"),
-#         bigquery.SchemaField("directors", "STRING", mode="REPEATED"),
-#         bigquery.SchemaField("writers", "STRING", mode="REPEATED"),
-#     ],
-#     "title.episode": [
-#         bigquery.SchemaField("tconst", "STRING"),
-#         bigquery.SchemaField("parentTconst", "STRING"),
-#         bigquery.SchemaField("seasonNumber", "INTEGER"),
-#         bigquery.SchemaField("episodeNumber", "INTEGER"),
-#     ],
-#     "title.principals": [
-#         bigquery.SchemaField("tconst", "STRING"),
-#         bigquery.SchemaField("ordering", "INTEGER"),
-#         bigquery.SchemaField("nconst", "STRING"),
-#         bigquery.SchemaField("category", "STRING"),
-#         bigquery.SchemaField("job", "STRING"),
-#         bigquery.SchemaField("characters", "STRING"),
-#     ],
-#     "title.ratings": [
-#         bigquery.SchemaField("tconst", "STRING"),
-#         bigquery.SchemaField("averageRating", "FLOAT"),
-#         bigquery.SchemaField("numVotes", "INTEGER"),
-#     ],
-# }
-
-
 def load_file_to_bigquery(project_id, bigquery_client, dataset_id, file_name, bucket):
+    """
+    Loads a file from GCS to a BigQuery table.
+
+    Args:
+        project_id (str): Google Cloud Project ID.
+        bigquery_client (bigquery.Client): Client for BigQuery service.
+        dataset_id (str): ID of the BigQuery dataset.
+        file_name (str): Name of the file to load.
+        bucket (storage.Bucket): GCS bucket containing the file.
+
+    Outputs:
+        Logs the completion of data loading to BigQuery.
+    """
     job_config = bigquery.LoadJobConfig(
-        # autodetect=True,
         field_delimiter="\t",
         skip_leading_rows=1,
         encoding="UTF-8",
@@ -145,37 +87,42 @@ def load_file_to_bigquery(project_id, bigquery_client, dataset_id, file_name, bu
         quote_character="",
     )
     uri = f"gs://{bucket.name}/{file_name}"
-    table_name = (
-        file_name.replace(".tsv.gz", "").replace(".", "_").replace("raw-datasets/", "")
-    )
+    table_name = file_name.replace(".tsv.gz", "").replace(".", "_").replace("raw-datasets/", "")
     table_id = f"{project_id}.{dataset_id}.{table_name}"
-
-    # Load the data into BigQuery
     load_job = bigquery_client.load_table_from_uri(uri, table_id, job_config=job_config)
     load_job.result()  # Wait for the job to complete
 
 
 @app.post("/load-data-to-bigquery/")
 async def load_data_to_bigquery():
-    load_dotenv()  # Load environment variables from the .env file.
+    """
+    Loads downloaded data from GCS to BigQuery.
 
-    # Get GCP project ID and BigQuery dataset ID from config
+    This endpoint facilitates the loading of IMDb dataset files from GCS into a
+    specified BigQuery dataset.
+
+    Raises:
+        HTTPException: If the GCP Project ID is missing.
+
+    Returns:
+        dict: Confirmation of successful data loading to BigQuery.
+    """
+    load_dotenv()
+
     project_id = Config.GOOGLE_CLOUD_PROJECT
-    dataset_id = "imdb_dataset"  # Customize as needed
+    dataset_id = "imdb_dataset"
 
     if not project_id:
         raise HTTPException(status_code=500, detail="GCP Project ID is not configured.")
 
-    # Initialize Google Cloud Storage and BigQuery clients
     storage_client = storage.Client(project=project_id)
     bigquery_client = bigquery.Client(project=project_id)
-
     project_hash = hashlib.blake2b(project_id.encode(), digest_size=8).hexdigest()
     bucket_name = f"{project_hash}-imdb-datasets"
     bucket = storage_client.bucket(bucket_name)
 
     for file_name in dataset_files:
-        file_name = f"raw-datasets/{file_name}"  # Add the subdirectory to the file name
+        file_name = f"raw-datasets/{file_name}"
         load_file_to_bigquery(
             project_id, bigquery_client, dataset_id, file_name, bucket
         )
--- src/setup/setup_gcs.py
+++ src/setup/setup_gcs.py
@@ -8,45 +8,38 @@ from google.cloud import bigquery, storage
 
 def create_bucket_and_directories(bucket_name_suffix):
     """
-    Creates a Google Cloud Storage (GCS) bucket and predefined directories within it.
+    Creates a GCS bucket and directories using a hashed project ID.
 
-    This function uses the hashed value of the Google Cloud Project ID to ensure the
-    bucket name remains unique yet consistently reproducible across multiple invocations.
+    The function uses the hashed value of the GCP project ID to ensure the
+    bucket name is unique and reproducible across multiple invocations.
 
     Args:
-        bucket_name_suffix (str): A suffix to append to the bucket name to ensure uniqueness.
+        bucket_name_suffix (str): Suffix for the bucket name to ensure uniqueness.
 
     Raises:
         ValueError: If the GCP project ID is not set in the environment.
 
     Outputs:
-        Logs the status of bucket and directory creation to the console.
+        Logs the status of bucket and directory creation.
     """
-    # Load environment variables
     load_dotenv()
 
-    # Get GCP project ID from environment
     project_id = Config.GOOGLE_CLOUD_PROJECT
     if not project_id:
         raise ValueError("GCP_PROJECT_ID is not set in the environment.")
 
-    # Generate a hash of the project ID to use in the bucket name
     project_hash = hashlib.blake2b(project_id.encode(), digest_size=8).hexdigest()
     bucket_name = f"{project_hash}-{bucket_name_suffix}"
 
-    # Initialize GCS client
     client = storage.Client(project=project_id)
 
-    # Check if the bucket already exists
     bucket = client.lookup_bucket(bucket_name)
     if not bucket:
-        # Create new bucket
         bucket = client.create_bucket(bucket_name)
         print(f"Bucket {bucket_name} created.")
     else:
         print(f"Bucket {bucket_name} already exists.")
 
-    # Directories to create within the bucket
     directories = ["raw-datasets/", "processed-datasets/", "logs/"]
     for directory in directories:
         blob = bucket.blob(directory)
@@ -56,81 +49,67 @@ def create_bucket_and_directories(bucket_name_suffix):
 
 def create_bigquery_dataset(dataset_id):
     """
-    Ensures the existence of a BigQuery dataset.
+    Ensures the existence of a specified BigQuery dataset.
 
-    The function checks for the existence of a specified dataset and creates it if it
-    does not exist, helping maintain data organization within BigQuery.
+    This function checks and creates a BigQuery dataset if it does not already
+    exist, aiding in data organization.
 
     Args:
-        dataset_id (str): The identifier for the dataset to check or create.
+        dataset_id (str): Identifier for the dataset to check or create.
 
     Raises:
         ValueError: If the GCP project ID is not set in the environment.
 
     Outputs:
-        Logs the status of dataset existence or creation to the console.
+        Logs the dataset's status (existence or creation).
     """
-    # Load environment variables
     load_dotenv()
 
-    # Get GCP project ID from environment
     project_id = Config.GOOGLE_CLOUD_PROJECT
     if not project_id:
         raise ValueError("GCP_PROJECT_ID is not set in the environment.")
 
-    # Initialize BigQuery client
     client = bigquery.Client(project=project_id)
 
-    # Check if the dataset already exists
     dataset_ref = client.dataset(dataset_id)
     try:
         client.get_dataset(dataset_ref)
         print(f"Dataset {dataset_id} already exists.")
     except:
-        # Create the dataset if it doesn't exist
         dataset = bigquery.Dataset(dataset_ref)
-        dataset = client.create_dataset(
-            dataset, timeout=30
-        )  # Added timeout parameter for API call
+        dataset = client.create_dataset(dataset, timeout=30)
         print(f"Dataset {dataset_id} created.")
 
 
 def create_bigquery_tables(dataset_id):
     """
-    Creates BigQuery tables within the specified dataset based on the IMDb dataset schema.
+    Creates tables in a specified BigQuery dataset according to IMDb schema.
 
     Args:
-        dataset_id (str): The identifier of the dataset where the tables will be created.
+        dataset_id (str): Identifier of the dataset for table creation.
 
     Raises:
         ValueError: If the GCP project ID is not set in the environment.
 
     Outputs:
-        Logs the status of table creation to the console.
+        Logs the status of table creation.
     """
-    # Load environment variables
     load_dotenv()
 
-    # Get GCP project ID from environment
     project_id = Config.GOOGLE_CLOUD_PROJECT
     if not project_id:
         raise ValueError("GCP_PROJECT_ID is not set in the environment.")
 
-    # Initialize BigQuery client
     client = bigquery.Client(project=project_id)
 
-    # Define the schema for each table
     table_schemas = {
         "name_basics": [
             bigquery.SchemaField("nconst", "STRING"),
             bigquery.SchemaField("primaryName", "STRING"),
             bigquery.SchemaField("birthYear", "INTEGER"),
             bigquery.SchemaField("deathYear", "INTEGER"),
-            # bigquery.SchemaField("primaryProfession", "STRING", mode="REPEATED"),
             bigquery.SchemaField("primaryProfession", "STRING"),
-            # bigquery.SchemaField("knownForTitles", "STRING", mode="REPEATED"),
             bigquery.SchemaField("knownForTitles", "STRING"),
-        
         ],
         "title_akas": [
             bigquery.SchemaField("titleId", "STRING"),
@@ -138,9 +117,7 @@ def create_bigquery_tables(dataset_id):
             bigquery.SchemaField("title", "STRING"),
             bigquery.SchemaField("region", "STRING"),
             bigquery.SchemaField("language", "STRING"),
-            # bigquery.SchemaField("types", "STRING", mode="REPEATED"),
             bigquery.SchemaField("types", "STRING"),
-            # bigquery.SchemaField("attributes", "STRING", mode="REPEATED"),
             bigquery.SchemaField("attributes", "STRING"),
             bigquery.SchemaField("isOriginalTitle", "BOOLEAN"),
         ],
@@ -153,15 +130,12 @@ def create_bigquery_tables(dataset_id):
             bigquery.SchemaField("startYear", "INTEGER"),
             bigquery.SchemaField("endYear", "INTEGER"),
             bigquery.SchemaField("runtimeMinutes", "INTEGER"),
-            # bigquery.SchemaField("genres", "STRING", mode="REPEATED"),
             bigquery.SchemaField("genres", "STRING"),
         ],
         "title_crew": [
             bigquery.SchemaField("tconst", "STRING"),
             bigquery.SchemaField("directors", "STRING"),
-            # bigquery.SchemaField("directors", "STRING", mode="REPEATED"),
             bigquery.SchemaField("writers", "STRING"),
-            # bigquery.SchemaField("writers", "STRING", mode="REPEATED"),
         ],
         "title_episode": [
             bigquery.SchemaField("tconst", "STRING"),
@@ -184,7 +158,6 @@ def create_bigquery_tables(dataset_id):
         ],
     }
 
-    # Create tables within the dataset
     for table_id, schema in table_schemas.items():
         table_ref = client.dataset(dataset_id).table(table_id)
         table = bigquery.Table(table_ref, schema=schema)
@@ -193,9 +166,9 @@ def create_bigquery_tables(dataset_id):
 
 
 if __name__ == "__main__":
-    bucket_name_suffix = "imdb-datasets"  # Customize as needed
+    bucket_name_suffix = "imdb-datasets"
     create_bucket_and_directories(bucket_name_suffix)
 
-    dataset_id = "imdb_dataset"  # Customize as needed
+    dataset_id = "imdb_dataset"
     create_bigquery_dataset(dataset_id)
     create_bigquery_tables(dataset_id)
